59c99454b6a791f1baed2dd406843eada517229a
Moroj-Alharbi <moroj.alh@gmail.com>
2018-12-11 16:55:21 +0300

    lab-done

--- controllers/termsController.js
+++ controllers/termsController.js
@@ -1,6 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
+var term = require('../models/term');
 
+router.get('/', term.getAll ,renderIndex);
+router.get('/new',renderNew);
+router.get('/:id/edit',term.find, renderEdit);
+router.get('/:id',term.find, renderShow);
+router.post('/',term.create, redirectShow);
+router.delete('/:id', term.delete, redirectIndex);
+router.put('/:id', term.update, redirectShow);
+
+function renderIndex(req, res){
+    var mustacheVariables = {
+        terms: res.locals.terms
+    }
+    res.render('./terms/index',mustacheVariables);
+}
+
+function renderShow(req, res){
+    var mustacheVariables =res.locals.term;
+    res.render('./terms/show',mustacheVariables);
+}
+
+function renderEdit(req, res){
+    var mustacheVariables = res.locals.term;
+    res.render('./terms/edit', mustacheVariables);
+}
+
+function renderNew(req, res){
+    res.render('./terms/new');
+}
+
+function redirectShow(req, res){
+    res.redirect(`/terms/${res.locals.term_id}`);
+}
+
+function redirectIndex(req, res){
+    res.redirect('/terms');
+}
 
 module.exports = router;
--- models/term.js
+++ models/term.js
@@ -1,5 +1,69 @@
 var db = require('../db/config');
 var term = {};
 
+term.getAll = function(req, res, next){
+    db.manyOrNone("SELECT * FROM terms;")
+    .then(function(result){
+        res.locals.terms = result;
+        next();
+    }).catch(function(err){
+        console.log(err);
+        next();
+    });
+}
+
+term.find = function(req, res, next){
+    var id = req.params.id;
+    db.oneOrNone("SELECT * FROM terms WHERE id=$1;", [id])
+    .then(function(result){
+        res.locals.term = result;
+        next();
+    }).catch(function(err){
+        console.log(err);
+        next();
+    })
+}
+
+term.create = function(req, res, next){
+    var termData = {
+        name: req.body.name,
+        definition: req.body.definition
+    }
+    db.one("INSERT INTO terms(name, definition) VALUES($1, $2) RETURNING id;",
+    [termData.name, termData.definition])
+    .then(function(result){
+        res.locals.term_id = result.id;
+        next();
+    }).catch(function(err){
+        console.log(err);
+        next();
+    })
+}
+
+term.update = function(req, res, next){
+    var termData = {
+        name: req.body.name,
+        definition: req.body.definition
+    }
+    db.one("UPDATE terms SET name= $1, definition= $2 WHERE id=$3 RETURNING id;",
+    [termData.name, termData.definition,req.params.id])
+    .then(function(result){
+        res.locals.term_id = result.id;
+        next();
+    }).catch(function(err){
+        console.log(err);
+        next();
+    })
+}
+
+term.delete = function(req, res, next){
+    db.none("DELETE FROM terms WHERE id=$1;",[req.params.id])
+    .then(function(){
+        next();
+    }).catch(function(err){
+        console.log(err);
+        next();
+    });
+}
 
 module.exports = term;
